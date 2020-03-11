1d3b5c8a9306005bfe881835eaf450b0753967d5
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-03-11 19:24:45 -0400

    Fixed connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 let mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysqlcreateConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
