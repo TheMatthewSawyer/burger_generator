e755843bb137c32f5a5b8cc48d58005c14335360
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-03-11 18:36:47 -0400

    Added gitignore file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
-INSERT INTO burgers (burger_name) VALUES ('Triple Burger');
-INSERT INTO burgers (burger_name) VALUES ('Regular Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Me Up Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cholesterol Champ');
+INSERT INTO burgers (burger_name) VALUES ('Wake and Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Murica Decuple Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Eaten Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stomached Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gone Burger', true);
+
 
 SELECT * FROM burgers;
